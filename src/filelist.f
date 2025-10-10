80940c374862fa16b794cf46fa76054c92aabe81
arjunporwal10 <40596253+arjunporwal10@users.noreply.github.com>
2025-10-10 14:51:25 -0700

    Fix filelist package order for YAML flow

--- src/filelist.f
+++ src/filelist.f
@@ -4,6 +4,7 @@
 +incdir+$ROOT/sequence_lib/src/
 +incdir+$ROOT/env/src/
 +incdir+$ROOT/yaml_flow
+$ROOT/src/apb_test_pkg.sv
 $ROOT/yaml_flow/yaml_flow_types_pkg.sv
 $ROOT/yaml_flow/vip_plugins_pkg.sv
 $ROOT/yaml_flow/stimulus_auto_builder_pkg.sv
@@ -11,5 +12,4 @@ $ROOT/yaml_flow/scenario_config_pkg.sv
 $ROOT/yaml_flow/action_executors_pkg.sv
 $ROOT/yaml_flow/yaml_flexible_seq_pkg.sv
 $ROOT/yaml_flow/yaml_tests_pkg.sv
-$ROOT/src/apb_test_pkg.sv
 $ROOT/src/example_test_pkg.sv
